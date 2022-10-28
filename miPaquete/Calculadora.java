bef66229096ed1fd89bdbc024ad1a2374d474166
José Carlos Ochoa Taboada <62448261+DrachenKaiser7@users.noreply.github.com>
2022-10-28 00:08:05 +0000

    MiCalculadora_18170412

--- miPaquete/Calculadora.java
+++ miPaquete/Calculadora.java
@@ -3,21 +3,22 @@ package miPaquete;
 public class Calculadora {
     public double suma(double x, double y)
     {
-        return 0;
+       
+        return x + y;
     }
 
     public double resta(double x, double y)
     {
-        return 0;
+        return x - y;
     }
 
     public double multiplica(double x, double y)
     {
-        return 0;
+        return x * y;
     }
 
     public double divide(double x, double y)
     {
-        return 0;
+        return (x/y);
     }
 }
