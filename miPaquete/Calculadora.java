1b8f31311aac66aa0cf4cb41ba4c1abf7917d5e0
davidols12 <116851084+davidols12@users.noreply.github.com>
2022-10-28 00:04:27 +0000

    Progamando mi calculadora 21170303

--- miPaquete/Calculadora.java
+++ miPaquete/Calculadora.java
@@ -1,23 +1,28 @@
 package miPaquete;
 
 public class Calculadora {
+    double r;
     public double suma(double x, double y)
     {
-        return 0;
+        r=x+y;
+        return r;
     }
 
     public double resta(double x, double y)
     {
-        return 0;
+        r=x-y;
+        return r;
     }
 
     public double multiplica(double x, double y)
     {
-        return 0;
+        r=x*y;
+        return r;
     }
 
     public double divide(double x, double y)
     {
-        return 0;
+        r=x/y;
+        return r;
     }
 }
